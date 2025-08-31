51eb9bab3334364c1b13a4fcd843ac4eb0288492
なえざつく <naezatsuku653@gmail.com>
2025-08-31 12:22:31 +0900

    時間の関係

--- src/app/info/Circle/ClockArc.tsx
+++ src/app/info/Circle/ClockArc.tsx
@@ -70,7 +70,7 @@ export default function AnimatedClockArc({ minutes }: { minutes: number }) {
       animate();
     }, [minutes]);
     return (
-      <svg viewBox="0 0 36 36" className="w-32 h-32 lg:w-48 lg:h-48">
+      <svg viewBox="0 0 36 36" className="w-24 sm:w-32 lg:w-40">
         <circle cx="18" cy="18" r="16" fill="none" stroke="#eee" strokeWidth="4" />
         {percent > 0 && (
           <circle
